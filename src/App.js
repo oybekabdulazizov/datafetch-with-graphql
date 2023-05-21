181dff3bbd1c387b57de0a92de5bd44beb7108fc
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-05-21 22:37:11 +0200

    move hooks into a hooks file

--- src/App.js
+++ src/App.js
@@ -1,89 +1,21 @@
-import { useState } from 'react';
-import { gql, useQuery } from '@apollo/client';
+import useAppState from './hooks/useAppState';
 
 import './App.css';
-import axios from 'axios';
-
-const GET_COUNTRIES = gql`
-  {
-    continents {
-      code
-      name
-    }
-    countries {
-      code
-      name
-      continent {
-        code
-        name
-      }
-    }
-    languages {
-      code
-      name
-    }
-  }
-`;
 
 function App() {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [selectedContinent, setSelectedContinent] = useState('');
-  const [number, setNumber] = useState(
-    Math.floor(Math.random() * (10 - 2) + 2)
-  );
-  const [countriesRes, setCountriesRes] = useState([]);
-
-  const toggleDropdown = () => setDropdownOpen((prevState) => !prevState);
-
-  const handleNumberChange = (e) => {
-    setNumber(e.target.value);
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    if (selectedContinent === '') {
-      alert('Please choose a continent!');
-      return;
-    }
-
-    const selectedContinentCountries = data.countries.filter(
-      (country) => country.continent.name === selectedContinent
-    );
-
-    const randomCountries = [];
-
-    for (let index = 0; index < number; index++) {
-      let randIdx = Math.floor(
-        Math.random() * selectedContinentCountries.length
-      );
-
-      function duplicateExists(countryWithDetails) {
-        return randomCountries.some(
-          ({ name }) => name['common'] === countryWithDetails.name['common']
-        );
-      }
-
-      const randCountry = selectedContinentCountries[randIdx];
-      let countryWithDetails;
-      let isDuplicate = true;
-      while (isDuplicate) {
-        countryWithDetails = await axios
-          .get(`https://restcountries.com/v3.1/name/${randCountry.name}`)
-          .then((res) => {
-            return res.data[0];
-          })
-          .catch((err) => {
-            return err;
-          });
-        isDuplicate = duplicateExists(countryWithDetails);
-      }
-      randomCountries.push(countryWithDetails);
-    }
-    setCountriesRes([...randomCountries]);
-  };
-
-  const { loading, error, data } = useQuery(GET_COUNTRIES);
+  const {
+    countriesRes,
+    data,
+    dropdownOpen,
+    error,
+    loading,
+    handleNumberChange,
+    handleSubmit,
+    number,
+    selectedContinent,
+    setSelectedContinent,
+    toggleDropdown,
+  } = useAppState();
 
   if (loading) return <h2>Loading...</h2>;
   if (error) return <h2>Error! {error.message}</h2>;
