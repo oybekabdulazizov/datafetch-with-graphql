0ed2c580e3064d048c7914172761371b5364d367
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-05-21 20:58:53 +0200

    set up a dropdown form and make sure it is working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { gql, useQuery } from '@apollo/client';
+
 import './App.css';
 
 const GET_COUNTRIES = gql`
@@ -23,15 +25,57 @@ const GET_COUNTRIES = gql`
 `;
 
 function App() {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [selectedContinent, setSelectedContinent] =
+    useState('Choose a continent');
+
+  const toggleDropdown = () => setDropdownOpen((prevState) => !prevState);
+
   const { loading, error, data } = useQuery(GET_COUNTRIES);
 
   if (loading) return <h2>'Loading...'</h2>;
   if (error) return <h2>`Error! ${error.message}`</h2>;
-  data.continents.map((continent) => {
-    console.log(continent.name);
-  });
+  const { continents, ountries } = data;
 
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <form>
+        <div className='dropdown-container'>
+          <div className='dropdown-header' onClick={toggleDropdown}>
+            {selectedContinent}
+          </div>
+          {dropdownOpen && (
+            <div className='dropdown-list'>
+              <div
+                onClick={() => {
+                  setSelectedContinent('option#1');
+                  toggleDropdown();
+                }}
+              >
+                Option #1
+              </div>
+              <div
+                onClick={() => {
+                  setSelectedContinent('option#2');
+                  toggleDropdown();
+                }}
+              >
+                Option #2
+              </div>
+              <div
+                onClick={() => {
+                  setSelectedContinent('option#3');
+                  toggleDropdown();
+                }}
+              >
+                Option #3
+              </div>
+            </div>
+          )}
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default App;
