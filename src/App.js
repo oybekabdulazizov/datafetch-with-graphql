9cd49ae9d8ea9c1510cf9e972a6dc08942fc1f5b
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-05-21 20:46:20 +0200

    install apollo/client, axios, and graphql depedencies, and do a test data fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
+import { gql, useQuery } from '@apollo/client';
 import './App.css';
 
+const GET_COUNTRIES = gql`
+  {
+    continents {
+      code
+      name
+    }
+    countries {
+      code
+      name
+      continent {
+        code
+        name
+      }
+    }
+    languages {
+      code
+      name
+    }
+  }
+`;
+
 function App() {
+  const { loading, error, data } = useQuery(GET_COUNTRIES);
+
+  if (loading) return <h2>'Loading...'</h2>;
+  if (error) return <h2>`Error! ${error.message}`</h2>;
+  data.continents.map((continent) => {
+    console.log(continent.name);
+  });
+
   return <div className='App'></div>;
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 import App from './App';
 
 import './index.css';
 
+const client = new ApolloClient({
+  uri: 'https://countries.trevorblades.com/graphql',
+  cache: new InMemoryCache(),
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
